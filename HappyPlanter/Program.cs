40be03f02a36b3d7e4e87814b7e4b84e6b06699c
Sara Jirholm <sara.jirholm@hotmail.com>
2020-04-26 15:17:37 +0200

    Make first succesful call to database

--- HappyPlanter/Program.cs
+++ HappyPlanter/Program.cs
@@ -1,26 +1,48 @@
 using System;
 using System.Collections.Generic;
+// using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Data.SqlClient;
+using Npgsql;
+using Dapper;
 
 namespace HappyPlanter
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+  class Plant
+  {
+    public int PlantId { get; set; }
+    public string Name { get; set; }
+    public DateTime LastWatered { get; set; }
+    public string Species { get; set; }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+  }
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var test = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+      var connection = new NpgsqlConnection(test);
+
+      connection.Open();
+
+      var selectAllFromPlants = "SELECT * FROM Plants";
+      var plants = connection.Query<Plant>(selectAllFromPlants).ToList();
+
+      plants.ForEach(plant => { Console.WriteLine(plant.Name); });
+
+      CreateHostBuilder(args).Build().Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+              webBuilder.UseStartup<Startup>();
+            });
+  }
 }
