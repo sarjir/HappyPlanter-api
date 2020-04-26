760f1b72c2e7a9e310a012a398cef4c7b3ee8ed1
Sara Jirholm <sara.jirholm@hotmail.com>
2020-04-26 15:21:03 +0200

    Remove unnecessary dependencies

--- HappyPlanter/Program.cs
+++ HappyPlanter/Program.cs
@@ -1,13 +1,11 @@
 using System;
 using System.Collections.Generic;
-// using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Data.SqlClient;
 using Npgsql;
 using Dapper;
 
