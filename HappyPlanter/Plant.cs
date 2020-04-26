70699fab7b4a5587002baa99f28bdf24a2baa2ed
Sara Jirholm <sara.jirholm@hotmail.com>
2020-04-26 15:31:48 +0200

    Break out Plants to its own file

---
+++ HappyPlanter/Plant.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HappyPlanter {
+  class Plant {
+    public int PlantId { get; set; }
+    public string Name { get; set; }
+    public DateTime LastWatered { get; set; }
+    public string Species { get; set; }
+
+  }
+}
--- HappyPlanter/Program.cs
+++ HappyPlanter/Program.cs
@@ -2,45 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Npgsql;
-using Dapper;
+// using Plant;
 
-namespace HappyPlanter
-{
-  class Plant
-  {
-    public int PlantId { get; set; }
-    public string Name { get; set; }
-    public DateTime LastWatered { get; set; }
-    public string Species { get; set; }
+namespace HappyPlanter {
 
-  }
-  public class Program
-  {
-    public static void Main(string[] args)
-    {
-      var test = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
-      var connection = new NpgsqlConnection(test);
+  public class Program {
+    public static void Main (string[] args) {
+      var test = Environment.GetEnvironmentVariable ("DB_CONNECTION_STRING");
+      var connection = new NpgsqlConnection (test);
 
-      connection.Open();
+      connection.Open ();
 
       var selectAllFromPlants = "SELECT * FROM Plants";
-      var plants = connection.Query<Plant>(selectAllFromPlants).ToList();
+      var plants = connection.Query<Plant> (selectAllFromPlants).ToList ();
 
-      plants.ForEach(plant => { Console.WriteLine(plant.Name); });
+      plants.ForEach (plant => { Console.WriteLine (plant.Name); });
 
-      CreateHostBuilder(args).Build().Run();
+      CreateHostBuilder (args).Build ().Run ();
     }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-              webBuilder.UseStartup<Startup>();
-            });
+    public static IHostBuilder CreateHostBuilder (string[] args) =>
+      Host.CreateDefaultBuilder (args)
+      .ConfigureWebHostDefaults (webBuilder => {
+        webBuilder.UseStartup<Startup> ();
+      });
   }
 }
